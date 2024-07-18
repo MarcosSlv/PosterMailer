671742eab0328274cb8cdff20a1cbb1061eb88cf
Marcos Silva <112339881+MarcosSlv@users.noreply.github.com>
2024-07-18 17:16:06 -0300

    Update formSubmitHandler.js

--- frontend/public/js/formSubmitHandler.js
+++ frontend/public/js/formSubmitHandler.js
@@ -87,7 +87,7 @@ export function handleFormSubmit() {
 
         console.log('dataSheetFinal', dataSheetFinal)
 
-        const response = await fetch('http://44.212.23.26:3333/api/upload', {
+        const response = await fetch('http://44.201.136.185/api/upload', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
