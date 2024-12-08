201190ff33e6977aedd46af1e375d9f9002987d1
Marcos Silva <112339881+MarcosSlv@users.noreply.github.com>
2024-12-08 18:54:01 -0300

    Update app.ts

--- PosterMailer-back-end/src/app.ts
+++ PosterMailer-back-end/src/app.ts
@@ -7,12 +7,10 @@ import path from "path";
 export const app = express();
 
 app.use(cors({
-  origin: "https://postermailer.vercel.app/"
+  origin: "https://postermailer.vercel.app"
 }));
 
 app.use("/pdfs", express.static(path.resolve(__dirname, "../pdfs")));
-app.use('/static', express.static(path.join(__dirname, 'utils')));
-
 
 app.use(express.json({ limit: "20mb" }));
 
